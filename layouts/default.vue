23500534ddc8dce6901c08293f225097173a8ae5
tokuyama_shota <tokush1031@gmail.com>
2023-10-17 17:24:58 +0900

    tailwindcss header add

---
+++ layouts/default.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+<template>
+  <header class="w-full p-6 flex bg-blue-900 items-center gap-x-6 text-white">
+    <NuxtLink to="/">Home</NuxtLink>
+    <NuxtLink to="/todo">Todo</NuxtLink>
+  </header>
+  <main class="w-9/12 mx-auto my-0 h-screen text-center pt-32">
+    <slot />
+  </main>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1 class="text-3xl text-blue-500 font-bold underline pb-8">
+    アプリを勉強用にどんどん作成していくだけのWebサイトです。
+  </h1>
+  <p class="text-2xl text-blue-300 font-bold pb-8">
+    フロントエンドエンジニアとしてまだまだ技術力が足りないので、<br />スキルアップの為に作成しました。
+  </p>
+  <h2 class="text-2xl text-blue-500 font-bold pb-8">Skill</h2>
+  <ul>
+    <li>Nuxt3</li>
+    <li>TailwindCSS</li>
+    <li>TypeScript</li>
+  </ul>
+</template>
